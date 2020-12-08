9a8198791c4010a3b9124c54ff70583f948b4442
Nick <nickberezny@hotmail.com>
2020-12-08 18:07:33 -0500

    improved nav

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var test= new XMLHttpRequest();
 test.open('GET', 'pages/Test.html', true);
 
-function changeContent()
+
+
+function changeContent(name, id)
 {
-	$("#Content").load('pages/Test.html')
+	$("#Content").load('pages/' + name + '.html')
+	$(".active").removeClass('active')
+	$('#'+id).addClass('active')
+	
 }
