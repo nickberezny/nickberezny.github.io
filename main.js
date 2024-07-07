b145babe8f0d2b1b1cf77b6c40926a91fd545d2a
nickberezny <nickberezny@hotmail.com>
2024-07-07 17:03:08 -0400

    update

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var test= new XMLHttpRequest();
 test.open('GET', 'pages/Test.html', true);
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 $("#Content").load('pages/Research.html')
 $(".active").removeClass('active')
 $('#first').addClass('active')
 
 for (let i = 0; i < 1000; i++) {
   document.getElementById("Bunny").style.top=i+"px";
+  sleep(100);
 }
 
 
@@ -19,3 +29,4 @@ function changeContent(name, id)
 	$('#'+id).addClass('active')
 	
 }
+
