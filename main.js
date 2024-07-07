00aebe2ec8ddc7cd73d26ae59b895d42431416ba
nickberezny <nickberezny@hotmail.com>
2024-07-07 16:00:17 -0400

    update

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var test= new XMLHttpRequest();
 test.open('GET', 'pages/Test.html', true);
-changeContent('Research', 'first');
+
+$("#Content").load('pages/Research.html')
+$(".active").removeClass('active')
+$('#'+id).addClass('active')
 
 
 function changeContent(name, id)
 {
 	$("#Content").load('pages/' + name + '.html')
 	$(".active").removeClass('active')
-	$('#'+id).addClass('active')
+	$('#first').addClass('active')
 	
 }
